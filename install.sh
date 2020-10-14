5ead14ccc03a0a5aa9957c4d385cafb21edfcb9c
oktntko <oktntko@gmail.com>
2020-10-14 22:44:53 +0900

    コマンド存在チェック

--- install.sh
+++ install.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 # Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+if !(type "brew" > /dev/null 2>&1); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
-brew doctor
+  brew doctor
+fi
 
 # zsh
-brew install zsh
-which zsh | sudo tee -a /etc/shells
+if !(type "zsh" > /dev/null 2>&1); then
+  brew install zsh
+  which zsh | sudo tee -a /etc/shells
+fi
 
 ## plugins
 ### zplug
-brew install zplug
+if !(type "zplug" > /dev/null 2>&1); then
+  brew install zplug
+fi
 
 ### vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ### nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+if !(type "nvm" > /dev/null 2>&1); then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+fi
 
 # link
 zsh "${ZDOTDIR:-$HOME}"/.dotfiles/link.zsh
