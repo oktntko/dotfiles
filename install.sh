9f44084332a0da16bbb205ea5501ef21b138f235
oktntko <oktntko@gmail.com>
2020-11-05 01:23:56 +0900

    link

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# https://docs.brew.sh/Homebrew-on-Linux
+# build-essential curl file git
+
+# ------------------------------------------------------------------------------
 # Homebrew
+# ------------------------------------------------------------------------------
 if !(type "brew" > /dev/null 2>&1); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
@@ -11,44 +16,54 @@ if !(type "brew" > /dev/null 2>&1); then
   brew doctor
 fi
 
+# ------------------------------------------------------------------------------
+# package install
+# ------------------------------------------------------------------------------
 # zsh
 if !(type "zsh" > /dev/null 2>&1); then
   brew install zsh
   which zsh | sudo tee -a /etc/shells
 fi
 
-## plugins
-### zplug
+# zplug
 if !(type "zplug" > /dev/null 2>&1); then
   brew install zplug
 fi
 
-### tree
+# tree
 if !(type "tree" > /dev/null 2>&1); then
   brew install tree
 fi
 
-### bat
+# bat
 if !(type "bat" > /dev/null 2>&1); then
   brew install bat
 fi
 
-### ripgrep
+# ripgrep
 if !(type "ripgrep" > /dev/null 2>&1); then
   brew install ripgrep
 fi
 
-### vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# fzf
+if !(type "fzf" > /dev/null 2>&1); then
+  brew install fzf
+fi
 
-### nvm
+# nvm
 if !(type "nvm" > /dev/null 2>&1); then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 fi
 
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# ------------------------------------------------------------------------------
 # link
+# ------------------------------------------------------------------------------
 zsh "${ZDOTDIR:-$HOME}"/.dotfiles/link.zsh
 
+# ------------------------------------------------------------------------------
 # sh 変更して終了(パスワード聞かれる)
+# ------------------------------------------------------------------------------
 chsh -s /home/linuxbrew/.linuxbrew/bin/zsh
-
