2f6eefdb409ecad92d6ab4fbdeaf1108cf6f74e7
oktntko <oktntko@gmail.com>
2021-02-24 00:11:02 +0900

    seiri

--- link.zsh
+++ link.zsh
@@ -2,7 +2,6 @@
 
 local SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-local links=$(find "$SCRIPT_DIR"/links -type f | sed -e 's/\(^\* \|^ \)//g' | cut -d " " -f 1) &&
-echo $links | fzf --preview "bat --color=always --style=numbers,header,grid --line-range :500 {}" | while read item; do
-  ln -s "${(q)item}" "${ZDOTDIR:-$HOME}/.$(basename ${(q)item})"
+for link_file in $(find "$SCRIPT_DIR"/links -type f); do
+  ln -s "$link_file" "${ZDOTDIR:-$HOME}/.$(basename $link_file)"
 done
