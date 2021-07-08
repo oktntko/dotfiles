3205ab13b4584955738f403a45f977371c75642f
oktntko <oktntko@gmail.com>
2021-07-09 02:06:58 +0900

    arch wsl

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew install zplug tree bat ripgrep fzf git-delta exa
-
-# vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# chsh
-chsh -s /bin/zsh
---
+++ install.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+if type yay > /dev/null; then
+  yay -S tree bat ripgrep fzf git-delta-git exa
+elif type yay > /dev/null; then
+  brew install tree bat ripgrep fzf git-delta exa
+fi
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# chsh
+chsh -s /bin/zsh
