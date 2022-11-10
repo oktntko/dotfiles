4b34e6b05a88f556e33c6dd42058354d993ef60d
oktntko <72344657+oktntko@users.noreply.github.com>
2022-11-10 13:54:00 +0900

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -2,8 +2,6 @@ c#!/bin/zsh
 
 if type yay > /dev/null; then
   yay -S tree bat ripgrep fzf git-delta exa
-elif type apt > /dev/null; then
-  sudo apt install tree bat ripgrep fzf exa # TODO git-delta
 elif type brew > /dev/null; then
   brew install tree bat ripgrep fzf git-delta exa
 fi
