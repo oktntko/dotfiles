56c03754c248f5597de957a66f808c29d38d88d7
oktntko <oktntko@gmail.com>
2026-02-08 14:54:20 +0900

    [nvim] 基本の Ctrl ショートカット

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,32 @@
 
 local map = vim.keymap.set
 
+-- Delete without copying to clipboard 削除時にクリップボードにコピーしない
+map("n", "x", '"_x')
+map("n", "X", '"_X')
+map("n", "<Del>", '"_x')
+
+-- Standard Ctrl shortcuts like other applications
+-- Ctrl+C: Copy (yank)
+map("n", "<C-c>", "yy", { desc = "Copy line" })
+map("v", "<C-c>", "y", { desc = "Copy selection" })
+
+-- Ctrl+X: Cut
+map("n", "<C-x>", "dd", { desc = "Cut line" })
+map("v", "<C-x>", "d", { desc = "Cut selection" })
+
+-- Ctrl+V: Paste
+map("n", "<C-v>", "p", { desc = "Paste after cursor" })
+map({ "i", "c" }, "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
+
+-- Ctrl+Z: Undo
+map("n", "<C-z>", "u", { desc = "Undo" })
+map("i", "<C-z>", "<Cmd>undo<CR>", { desc = "Undo in insert mode" })
+
+-- Ctrl+Y: Redo
+map("n", "<C-y>", "<C-r>", { desc = "Redo" })
+map("i", "<C-y>", "<C-o><C-r>", { desc = "Redo in insert mode" })
+
 -- Add custom keymap to toggle Snacks Explorer
 map("n", "<C-t>", function()
   require("config.myfunction").toggle_explorer()
