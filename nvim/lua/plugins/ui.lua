8718c2c8bc75004b9b1804667b8ce27b9721d0bf
oktntko <oktntko@gmail.com>
2026-02-10 00:46:29 +0900

    [nvim] フォーカスを強調表示する

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -148,6 +148,26 @@ return {
         end,
       }
     end,
-  }
+  },
 
+  -- フォーカス外のウィンドウを暗くする
+  {
+    "levouh/tint.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- 暗さの度合い
+      tint = -30,       -- Darken colors, use a positive value to brighten
+      -- サチュレーション（彩度）を落とす設定
+      saturation = 0.6, -- Saturation to preserve
+
+      window_ignore_function = function(winid)
+        local bufid = vim.api.nvim_win_get_buf(winid)
+        local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+        local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+
+        -- Do not tint `terminal` or floating windows, tint everything else
+        return buftype == "terminal" or floating
+      end
+    },
+  },
 }
