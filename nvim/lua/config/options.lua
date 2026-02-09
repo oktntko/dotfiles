287b164b875b44bc06399b796269fe47e6d1b681
oktntko <oktntko@gmail.com>
2026-02-09 23:27:10 +0900

    [nvim] 行番号の相対表示を無効化する

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 -- 行末で <Right> => 次の行の行頭, 行頭で <Left> => 前の行の行末
 vim.opt.whichwrap:append("<,>,[,]")
+
+-- 行番号の相対表示を無効化する
+vim.opt.relativenumber = false
