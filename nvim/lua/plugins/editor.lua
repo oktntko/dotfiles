10a1ff6c70d8263fecd6f9900a3fa9c9d395bb4e
oktntko <oktntko@gmail.com>
2026-02-16 00:48:49 +0900

    [nvim] move

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,4 +1,28 @@
 return {
+  -- 入力補完
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "enter", -- Enterで確定する基本スタイル
+
+        -- Tabでメニューの最初の項目を選択し、次へと進む設定
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+      completion = {
+        list = {
+          selection = {
+            -- ここを 'preselect' にすると、メニューが出た瞬間に1番目が選択状態になります
+            -- 'manual' だと、Tabを1回叩いた時に1番目が選択されます
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+      },
+    },
+  },
+
   -- 1. LazyVim標準の mini.comment を無効化
   { "nvim-mini/mini.comment", enabled = false },
 
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,4 +1,17 @@
 return {
+
+  -- colorscheme
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- colorscheme は最優先で読み込む
+    config = function()
+      require("onedark").setup({
+        style = "cool",
+      })
+      require("onedark").load()
+    end,
+  },
+
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
