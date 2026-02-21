94a6ba61e294dc40ee105ad5b60347876549b688
oktntko <oktntko@gmail.com>
2026-02-22 01:20:05 +0900

    [nvim] fix: grug-far

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -54,4 +54,78 @@ return {
       })
     end,
   },
+
+  {
+    "MagicDuck/grug-far.nvim",
+    keys = {
+      {
+        "<C-S-r>",
+        function()
+          local function tab_has_grug(tab)
+            for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.bo[buf].filetype == "grug-far" then
+                return true
+              end
+            end
+            return false
+          end
+
+          -- grug-far に移動するとデフォルトで insert になっている。
+          -- トグルして戻ると元々が何のモードでも insert が残るので強制的に戻す。
+          vim.cmd("stopinsert")
+
+          local current_tab = vim.api.nvim_get_current_tabpage()
+
+          -- ① 現在タブにあるならタブごと閉じる
+          if tab_has_grug(current_tab) then
+            vim.cmd("tabclose")
+            return
+          end
+
+          -- ② 他タブにあるか探す
+          for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+            if tab ~= current_tab and tab_has_grug(tab) then
+              vim.api.nvim_set_current_tabpage(tab)
+              return
+            end
+          end
+
+          -- ③ どこにもなければ新規タブで開く
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            windowCreationCommand = "tab split",
+            openTargetWindow = {
+              preferredLocation = "right",
+            },
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+              flags = "--smart-case --word-regexp",
+              -- -w / --word-regexp    単語単位
+              -- -i / --ignore-case    大文字小文字無視
+              -- -s / --case-sensitive 常に区別
+              -- -S / --smart-case     大文字含むと区別
+            },
+          })
+        end,
+        mode = { "i", "n", "x" },
+        desc = "Toggle Search and Replace",
+      },
+    },
+    opts = function(_, opts)
+      opts.keymaps = opts.keymaps or {}
+
+      opts.keymaps.gotoLocation = {
+        n = "<localleader>o",
+      }
+      opts.keymaps.openLocation = {
+        n = "<localleader>e",
+      }
+      opts.keymaps.swapEngine = {
+        n = false,
+      }
+    end,
+  },
 }
