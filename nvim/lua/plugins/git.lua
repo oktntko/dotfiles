c978e95ef303a7344a8afdf5099d37876dcfa2a8
oktntko <oktntko@gmail.com>
2026-02-22 21:13:49 +0900

    [nvim] git graph

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -174,4 +174,32 @@ return {
       }
     end,
   },
+  {
+    "isakbm/gitgraph.nvim",
+    dependencies = { "sindrets/diffview.nvim" },
+    opts = {
+      hooks = {
+        -- Check diff of a commit
+        on_select_commit = function(commit)
+          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+        end,
+        -- Check diff from commit a -> commit b
+        on_select_range_commit = function(from, to)
+          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          require("gitgraph").draw({}, { all = true, max_count = 5000 })
+        end,
+        mode = { "n", "v" },
+        desc = "GitGraph - Draw",
+      },
+    },
+  },
 }
