09238edfcfaa5f692357666d6da877c52130ed91
oktntko <oktntko@gmail.com>
2026-03-09 23:14:27 +0900

    [nvim] off animation

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -365,20 +365,6 @@ return {
     opts = {},
   },
 
-  -- カーソル位置の移動を強調
-  {
-    "sphamba/smear-cursor.nvim",
-    opts = { -- Default  Range
-      -- stiffness = 0.8, -- 0.6      [0, 1]
-      trailing_stiffness = 1, -- 0.45     [0, 1]
-      stiffness_insert_mode = 1, -- 0.5      [0, 1]
-      trailing_stiffness_insert_mode = 1, -- 0.5      [0, 1]
-      -- damping = 0.95, -- 0.85     [0, 1]
-      damping_insert_mode = 1, -- 0.9      [0, 1]
-      -- distance_stop_animating = 0.5, -- 0.1      > 0
-    },
-  },
-
   -- 折り畳み、LSPの診断結果、Gitなどの情報を表示する
   {
     "luukvbaal/statuscol.nvim",
